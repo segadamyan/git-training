2309ffdaba895e445a458b0b9999e770d9b903bd
Sergey Adamyan <segadamyan@mail.ru>
2021-10-06 17:57:35 +0400

    hello all

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,7 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Hello " + args[i]);
+        }
     }
 }
